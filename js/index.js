e3709659446a5bfdcabda7c08f28adc11d4853f8
Maribel Duran <maribelduranucm@gmail.com>
2016-12-13 19:27:50 -0800

    Added promises to call the Twitch get users API. Used promises.all to verify all user channel info i  retrieved before calling the Twitch get streams API.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 //After all the channel requests have been completed, we create a string of active twitch users, and make a get stream request.
 //if the user is part of the returned stream array, then we gather the current stream info and updated the user to be online.
 //else we get the user's live stream  detail  
-var potentialUsers=["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "maribel"];
+var potentialUsers=["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "maribel", "fad"];
 
 var userList = {
   users: [],
@@ -29,11 +29,11 @@ var userList = {
   addLiveStreamer: function(name, details){
     //find the user in the array and add details property
     var streamerIndex = this.findUser(name);
-    console.log("streamerIndex is: ", streamerIndex);
+   // console.log("streamerIndex is: ", streamerIndex);
     if(streamerIndex != -1){
      this.users[streamerIndex].online = true;
-    this.users[streamerIndex].details = details;
-      
+     this.users[streamerIndex].details = details;
+
     }
   },
   
@@ -58,69 +58,84 @@ var user =  {
     details: ""
 }
 */
-getTwitchUsers(potentialUsers, getActiveUserStreams);
 
-function getTwitchUsers(potentialUsers, callback){
-    potentialUsers.forEach(function (userName){
-      getChannel(userName);
-    });
-   callback();
-}
-//need a callback function to make sure the above is finished first before running this
-//getActiveUserStreams(); 
+getTwitchUsers(potentialUsers);
 
-function getActiveUserStreams(){
-  var activeUsers = 'freecodecamp,ESL_SC2,OgamingSC2,cretetion,storbeck,habathcx,RobotCaleb,noobs2ninjas,maribel'; 
-  for (var user in userList.users){
-    //console.log(users);
-   // if (user.active){
-        //activeUsers = user.name + ','
-    //} 
-  }
-  //console.log("active user is:", activeUsers);
-  getStreams(activeUsers);
+
+//First time using Promises :))
+ function getTwitchUsers(potentialUsers){
+  var requests = [];
+   potentialUsers.forEach(function (userName){
+     
+    requests.push(getChannel(userName));
+      getChannel(userName).then(function(response) {
+      var channel = JSON.parse(response);
+    if (channel.error == "Not Found"){
+      userList.addInactiveUser(userName);
+        document.getElementById('error').innerHTML = channel.error;
+      }else{
+        document.getElementById('name').innerHTML = channel.name;
+        var channel_url = 'https://player.twitch.tv/?channel=' + channel.display_name;
+        userList.addActiveUser(channel.name, channel.display_name, channel.logo, channel_url);
+      }
+    }, function(error) {
+        console.error("Failed!", error);
+      
+      });
+ });
+   Promise.all(requests).then(function(results) {
+    //all promises have been resolved. We have retrieved all users information channel info. Now we can get active user stream
+    getActiveUserStreams();
+})
+.catch(function(error) {
+  // One or more promises was rejected
+});
   
 }
+
+function getActiveUserStreams(){
+ var activeUsers = '';
+  userList.users.forEach(function(user){
+    if (user.active){
+      activeUsers += user.name + ',';
+    }
+  });
+  getStreams(activeUsers);
+} 
 
 
 //Calls a Twitch.API GET channel request. Returns a channel object if userName is active
 function getChannel(userName){
-  var url = 'https://api.twitch.tv/kraken/channels/' + userName +
-      '?client_id=1fqby4hqnlm4h1t0ze5o5kgsn10k1z';
-  console.log(url);
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400)  { // Success!
-        var channel = JSON.parse(request.response);
-        document.getElementById('name').innerHTML = channel.name;
-      var channel_url = 'https://player.twitch.tv/?channel=' + channel.display_name;
-      userList.addActiveUser(channel.name, channel.display_name,
-      channel.logo, channel_url);
-       
-      }else{
-        if (request.status == 404){
-          //console.log("user channel not found");
-           var data = JSON.parse(request.response);
-          userList.addInactiveUser(userName);
-          //console.log(userList.users);
-           //console.log(data);
-          document.getElementById('error').innerHTML = data.message;
+  
+  return new Promise(function(resolve,reject){
+    var url = 'https://api.twitch.tv/kraken/channels/' + userName + '?client_id=1fqby4hqnlm4h1t0ze5o5kgsn10k1z';
+    //console.log(url);
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400)  { // Success!
+        resolve(request.response);
+        }else{
+          if (request.status == 404){
+           // console.log(request);
+           resolve(request.response);
+          }
+         // We reached our target server, but it returned an error
+            reject(Error(request.statusText));
         }
-       // We reached our target server, but it returned an error   
-      }
-    };
-    request.onerror = function() {
-      // There was a connection error of some sort
-    };
-    request.send();
+      };
+      request.onerror = function() {
+        // There was a connection error of some sort
+          reject(Error("Network Error"));
+      };
+      request.send();
+  });
 }
 
 function getStreams(users){
   var url = 'https://api.twitch.tv/kraken/streams?channel=' + users +
    '&client_id=1fqby4hqnlm4h1t0ze5o5kgsn10k1z';
   
-  console.log(url);
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.onload = function() {
@@ -128,15 +143,7 @@ function getStreams(users){
         var streams = JSON.parse(request.response).streams;
        streams.forEach(function(obj){
          userList.addLiveStreamer(obj.channel.name, obj.channel.status);
-        console.log("streamer is: ", obj.channel.name, "and current stream status is: ", obj.channel.status)
       });
-     console.log(userList.users);
-       /* document.getElementById('name').innerHTML = channel.name;
-      var channel_url = 'https://player.twitch.tv/?channel=' + channel.display_name;
-      userList.addActiveUser(channel.name, channel.display_name,
-      channel.logo, channel_url);
-      */
-       
       }else{
        // We reached our target server, but it returned an error   
       }
@@ -146,7 +153,3 @@ function getStreams(users){
     };
     request.send();
   }   
-
-//console.log(userList.users);
-//console.log(userList.addLiveStreamer('cretetion', "currentlyLive!"));
-
